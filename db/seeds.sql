793a15e842df50de0a1b178354f5a93aeacc70ce
Jesse Erickson <clerick44@hotmail.com>
2021-09-16 19:35:35 -0500

    initial commit, created schema.sql, server.js

---
+++ db/seeds.sql
@@ -0,0 +1,18 @@
+INSERT INTO department(name)
+VALUES  ("Sales")
+        ("Finance")
+        ("Engineering")
+        ("Legal")
+
+INSERT INTO role (title, salary, department_id)
+VALUES  ("Salesperson", 80000, 1)
+        ("Lead Engineer", 150000, 3)
+        ("Software Engineer", 90000, 3)
+        ("Account Manager", 100000, 2)
+        ("Accountant", 160000, 2)
+        ("Legal Team Lead", 190000, 4)
+        ("Lawyer", 50, 4)
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ("Jesse", "Erickson", 2)
+        ("Lisa", "Brown", 4)
